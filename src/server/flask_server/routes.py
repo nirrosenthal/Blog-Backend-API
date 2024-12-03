75d548cffc47e567c9a949bdfa2e4ad4fe8300a1
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2024-12-04 01:33:42 +0200

    Add error handling through main messages blueprint, all custom errors automatically generate a failed request json response

--- src/server/flask_server/routes.py
+++ src/server/flask_server/routes.py
@@ -1,23 +1,48 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, make_response
 from src.database.odm_blog import Post, Message
 from dataclasses import asdict
-from src.database.repository import Repository
+import src.database.repository as repository
 import src.server.flask_server.input_validation as Input_Validation
 from pydantic import ValidationError
+from src.server.flask_server.exceptions import InputValidationError, BlogAppException
 
 messages_bp = Blueprint('messages',__name__)
+
+@messages_bp.errorhandler(BlogAppException)
+def handle_blog_app_exception(exception:BlogAppException):
+    response = {
+        "error": type(exception).__name__,
+        "message": exception.message
+    }
+    return jsonify(response),exception.error_code
 
 
 @messages_bp.route('create', methods=['POST'])
 def create_message_blog():
     content = request.get_json().get('content', '')
     user_id_owner = request.get_json().get('user_id_owner','')
+    reply_to_message_id = request.get_json().get('reply_to_message_id','')
     try:
-        Input_Validation.MessageCreateRequest(user_id_owner=user_id_owner, content=content)
-        created_message: Message = Repository().create_message_blog(content, user_id_owner)
+        Input_Validation.MessageCreateRequest(user_id_owner=user_id_owner, content=content, reply_to_message_id=reply_to_message_id)
+
+        created_message: Message = repository.SERVER_REPOSITORY.create_message_blog(content=content, user_id_owner=user_id_owner, reply_to_message_id=reply_to_message_id)
         return jsonify(asdict(created_message)), 200
-    except ValidationError:
-        return jsonify({"error":"Validation error"})
+    except ValidationError or TypeError as e:
+        raise InputValidationError(str(e))
+
+
+
+@messages_bp.route('posts',methods=['GET'])
+def get_posts_blog():
+    start_index = int(request.get_json().get('start_index',0))
+    limit:int = int(request.get_json().get('limit',Input_Validation.POSTS_GET_LIMIT))
+    try:
+        Input_Validation.PostsGetRequest(start_index=start_index, posts_limit=limit)
+        posts:list[Post] = repository.SERVER_REPOSITORY.get_posts_blog(start_index=start_index, posts_limit=limit)
+        return jsonify(posts), 200
+    except ValidationError or TypeError as e:
+        raise InputValidationError(str(e))
+
 
 @messages_bp.route('edit',methods=['POST'])
 def edit_message_blog():
@@ -25,11 +50,12 @@ def edit_message_blog():
     content = request.get_json().get('content','')
     try:
         Input_Validation.MessageEditRequest(message_id=message_id, content=content)
-        edited_message:Message = Repository().edit_message_blog(message_id, content)
+        edited_message:Message = repository.SERVER_REPOSITORY.edit_message_blog(message_id, content)
 
         return jsonify(asdict(edited_message)), 200
-    except ValidationError:
-        return jsonify({"error":"Validation Error"})
+    except ValidationError as e:
+        raise InputValidationError(str(e))
+
 
 
 @messages_bp.route('delete', methods=['DELETE'])
@@ -37,10 +63,10 @@ def delete_message_blog():
     message_id = request.get_json().get('message_id','')
     try:
         Input_Validation.MessageDeleteRequest(message_id)
-        deleted_message:Message = Repository().delete_message_blog(message_id)
+        deleted_message:Message = repository.SERVER_REPOSITORY.delete_message_blog(message_id)
         return jsonify(asdict(deleted_message)), 200
-    except ValidationError:
-        return jsonify({"error":"Validation Error"})
+    except ValidationError as e:
+        raise InputValidationError(str(e))
 
 
 @messages_bp.route('like/add', methods=['PUT'])
@@ -49,17 +75,20 @@ def add_message_like():
     user_id = "need to get curr user id"
     try:
         Input_Validation.MessageLikeRequest(message_id,user_id)
-        Repository().add_message_like(message_id, user_id)
-    except ValidationError:
-        return jsonify({"error":"Validation Error"})
+        repository.SERVER_REPOSITORY.add_message_like(message_id, user_id)
+        return make_response('', 204)
+    except ValidationError as e:
+        raise InputValidationError(str(e))
 
 
-@messages_bp.route('like/add', methods=['PUT'])
+@messages_bp.route('like/remove', methods=['PUT'])
 def remove_message_like():
     message_id = request.get_json().get('message_id','')
     user_id = "need to get curr user id"
     try:
         Input_Validation.MessageLikeRequest(message_id,user_id)
-        Repository().remove_message_like(message_id, user_id)
-    except ValidationError:
-        return jsonify({"error":"Validation Error"})
+        repository.SERVER_REPOSITORY.remove_message_like(message_id, user_id)
+        return make_response('', 204)
+    except ValidationError as e:
+        raise InputValidationError(str(e))
+
