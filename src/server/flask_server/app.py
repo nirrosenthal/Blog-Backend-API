ce05c0f63f805cd84638ccce6f9865bfe411d909
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2024-12-03 22:11:55 +0200

    add blueprint for messages

--- src/server/flask_server/app.py
+++ src/server/flask_server/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from src.server.flask_server.routes import messages_bp
+from src.database.mongo_db.mongo_repository import MongoDBRepository
 
 app = Flask(__name__)
 
@@ -6,5 +8,13 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+app.register_blueprint(messages_bp, url_prefix='/api/v0/messages')
+
 if __name__ == '__main__':
+    MongoDBRepository()
     app.run(debug=True)
+
+
+
+
+
