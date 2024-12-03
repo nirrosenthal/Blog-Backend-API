65fb631236a9a12779e16c2fa3e1ffacf88125af
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2024-12-04 01:32:30 +0200

    Add GetPostRequest input validation, including message limit

--- src/server/flask_server/input_validation.py
+++ src/server/flask_server/input_validation.py
@@ -1,30 +1,56 @@
+from bson import ObjectId
+from bson.errors import InvalidId
 from pydantic import BaseModel, Field, ValidationError, validator
+import src.database.repository as repository
+from src.server.flask_server.exceptions import InputValidationError
 
-INPUT_LENGTH_LIMIT = 1000
+INPUT_LENGTH_LIMIT:int = 1000
+POSTS_GET_LIMIT:int = 1000
+
+class MessageId(BaseModel):
+    message_id: str = Field(...,min_length=1, max_length=INPUT_LENGTH_LIMIT)
+    @validator('message_id')
+    def validate_message_id(cls, message_id):
+        try:
+            ObjectId(message_id)
+        except InvalidId as e:
+            raise InputValidationError(str(e))
+
+class PostsGetRequest(BaseModel):
+    start_index:int = Field(...,ge=0)
+    posts_limit:int = Field(...,ge=0,le=POSTS_GET_LIMIT)
 
 
 class MessageGetRequest(BaseModel):
-    message_id: str = Field(...,min_length=1, max_length=INPUT_LENGTH_LIMIT)
     user_id: str = ''
 
 
 class MessageCreateRequest(BaseModel):
     content: str = Field(...,min_length=1, max_length=INPUT_LENGTH_LIMIT)
     user_id_owner: str = Field(...,min_length=1, max_length=INPUT_LENGTH_LIMIT)
-    reply_to_message_id: str =  Field(...,min_length=1, max_length=INPUT_LENGTH_LIMIT)
+    reply_to_message_id:str = None
+    @validator('reply_to_message_id')
+    def message_id_exists(cls, reply_to_message_id):
+        try:
+            if reply_to_message_id != '':
+                ObjectId(reply_to_message_id)
+                repository.SERVER_REPOSITORY.get_message_blog(reply_to_message_id)
+            return reply_to_message_id
+        except InvalidId as e:
+            raise InputValidationError(str(e))
 
 
 class MessageEditRequest(BaseModel):
-    message_id: str = Field(...,min_length=1, max_length=INPUT_LENGTH_LIMIT)
+    message_id:str = MessageId
     content: str = Field(...,min_length=1, max_length=INPUT_LENGTH_LIMIT)
 
 
 class MessageDeleteRequest(BaseModel):
-    message_id: str = Field(...,min_length=1, max_length=INPUT_LENGTH_LIMIT)
+    message_id:str =  MessageId
 
 
 class MessageLikeRequest(BaseModel):
-    message_id: str = Field(...,min_length=1, max_length=INPUT_LENGTH_LIMIT)
+    message_id: MessageId
     user_id: str = Field(...,min_length=1, max_length=INPUT_LENGTH_LIMIT)
 
 
