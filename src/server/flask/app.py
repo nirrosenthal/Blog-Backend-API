72d99430826d38f9f2c5b8260b5e3246c0ae2001
ngrvendeta <74234047+nirrosenthal@users.noreply.github.com>
2024-12-06 11:17:09 +0200

    remove commented code

--- src/server/flask/app.py
+++ src/server/flask/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_login import LoginManager
 import os
 from src.server.routes.messages import messages_bp
-import src.database.repository as repository
-from src.database.mongo_db.mongo_repository import MongoDBRepository
+import src.db.repository as repository
+from src.db.mongo_db.mongo_repository import MongoDBRepository
 from src.server.routes.auth import auth_bp
 import logging
 
@@ -18,19 +18,10 @@ def home():
 app.register_blueprint(messages_bp, url_prefix='/api/v0/messages')
 app.register_blueprint(auth_bp, url_prefix='/api/v0/auth')
 
-
 @app.cli.command("init-repository")
 def init_database_repository():
     repository.SERVER_REPOSITORY = MongoDBRepository()
     logging.info(f"Database Repository Initiated")
-#
-#
-# def flask_app_run():
-#     """Custom run command to make sure repository will run first"""
-#     init_database_repository()
-#     app.run(host="0.0.0.0", port=int(os.getenv("FLASK_PORT", 5000)))
-#     logging.info(f"Flask app started running on port {os.getenv('FLASK_PORT')}")
-#     return app
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.getenv("FLASK_PORT", 5000)))
